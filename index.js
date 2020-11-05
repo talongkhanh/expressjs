ee5415e42d1868aa55c542ea74747d43787c8fc1
Yuno <59208657+talongkhanh@users.noreply.github.com>
2020-11-05 15:28:53 +0700

    request query

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const path = require('path');
 const app = express();
 const port = 3001;
 
+const users = [
+			{ id: 1, name: 'Minh' },
+			{ id: 2, name: 'Nam' },
+];
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -12,15 +17,28 @@ app.get('/', (req, res) => {
 		name: 'Khanh', 
 		age: 20,
 	});
-})
+});
 
 app.get('/users', (req, res) => {
 	res.render('users/index', {
-		users: [
-			{ id: 1, name: 'Minh' },
-			{ id: 2, name: 'Nam' },
-		],
+		users: users,
 	});
+});
+
+app.get('/users/search', (req, res) => {
+	const q = req.query.q;
+	if(q) {
+		const matchedUsers = users.filter(user => {
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+		});
+		res.render('users/index', {
+			users: matchedUsers,
+			q: q,
+		});
+	} else {
+		res.redirect('/users');
+	}
+	
 })
 
 
