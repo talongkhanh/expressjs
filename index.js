11940da4b1da9d38da6c23ed9a5fb5e05b9ba703
Yuno <59208657+talongkhanh@users.noreply.github.com>
2020-11-07 03:05:00 +0700

    express router

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const low = require('lowdb');
-const shortId = require('shortid');
 
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('db.json');
-const db = low(adapter);
- 
-db.defaults({ users: [] })
-  .write();
+const route = require('./routes');
 
 const app = express();
 const port = 3001;
@@ -21,6 +14,8 @@ app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+route(app);
+
 app.get('/', (req, res) => {
 	res.render('index', { 
 		name: 'Khanh', 
@@ -28,46 +23,6 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.get('/users', (req, res) => {
-	res.render('users/index', {
-		users: db.get('users').value(),
-	});
-});
 
-app.get('/users/create', (req, res) => {
-	res.render('users/create');
-});
-
-app.post('/users/create', (req, res) => {
-	if(req.body.name) {
-		req.body.id = shortId.generate();
-		db.get('users').push(req.body).write();
-	}
-	res.redirect('/users');
-});
-
-app.get('/users/search', (req, res) => {
-	const q = req.query.q;
-	if(q) {
-		const matchedUsers = db.get('users').value().filter(user => {
-		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-		});
-		res.render('users/index', {
-			users: matchedUsers,
-			q: q,
-		});
-	} else {
-		res.redirect('/users');
-	}
-	
-});
-
-app.get('/users/:userId', (req, res) => {
-	const id = req.params.userId;
-	const user = db.get('users').find({id: id}).value();
-	res.render('users/show', {
-		user: user,
-	});
-});
 
 app.listen(port, () => console.log('Server listen on port ', port));
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const userRouter = require('./user');
+
+function route(app) {
+	app.use('/users', userRouter);
+}
+
+module.exports = route;
