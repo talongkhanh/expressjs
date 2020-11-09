a5b79121c69053343af68a06dfc5170dfeba7b06
Yuno <59208657+talongkhanh@users.noreply.github.com>
2020-11-09 21:11:29 +0700

    express static files

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const route = require('./routes');
 const app = express();
 const port = 3001;
 
-app.use(morgan('tiny'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
